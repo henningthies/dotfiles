8e63eaf50806815843a22b5a022fd3d3c672d607
Henning Thies <mail@henning-thies.de>
2022-12-12 19:50:44 +0100

    enable completion with tap/s-tab

--- nvim/.config/nvim/lua/completion.lua
+++ nvim/.config/nvim/lua/completion.lua
@@ -62,7 +62,9 @@ cmp.setup({
   },
   mapping = {
     ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
     ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<CR>"] = cmp.mapping.confirm(),
