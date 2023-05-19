f290257e63a8844dff94fe1b131625f372b5fcff
Henning Thies <mail@henning-thies.de>
2023-05-19 07:36:43 +0200

    remove obsolete mappings for nvimtree

--- nvim-lua/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim-lua/.config/nvim/lua/plugins/nvim-tree.lua
@@ -5,11 +5,6 @@ return {
       sort_by = "case_sensitive",
       view = {
         width = 30,
-        mappings = {
-          list = {
-            { key = "u", action = "dir_up" },
-          },
-        },
       },
       renderer = {
         group_empty = true,
