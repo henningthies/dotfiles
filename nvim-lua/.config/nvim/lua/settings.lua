37385176ced08d6259850875d106ce64f9f1d66e
Henning Thies <mail@henning-thies.de>
2023-06-12 23:25:28 +0200

    nvim: no linenumbers but signcolumn

--- nvim-lua/.config/nvim/lua/settings.lua
+++ nvim-lua/.config/nvim/lua/settings.lua
@@ -1,6 +1,7 @@
-vim.o.number = true
+vim.o.number = false
 vim.o.encoding = "UTF-8"
 vim.o.relativenumber = false
+vim.o.signcolumn = "yes"
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
