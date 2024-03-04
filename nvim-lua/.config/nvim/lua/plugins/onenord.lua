e657f1a27338c8f01546e0ce11d725ab660b3e73
Henning Thies <mail@henning-thies.de>
2024-03-04 08:50:08 +0100

    add custom highlights hint to onenord

--- nvim-lua/.config/nvim/lua/plugins/onenord.lua
+++ nvim-lua/.config/nvim/lua/plugins/onenord.lua
@@ -1,8 +1,12 @@
 return {
   'rmehri01/onenord.nvim',
   config = function()
+    --local colors = require("onenord.colors").load()
     require('onenord').setup({
       theme = nil, -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+      custom_highlights = {
+        --["@constructor"] = { fg = colors.dark_blue },
+      },
     })
   end,
 }
