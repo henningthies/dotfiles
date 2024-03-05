f62092c4d4671ccf4d52bc361834aaee912a889b
Henning Thies <mail@henning-thies.de>
2024-03-05 07:43:32 +0100

    only trigger cmp completion after 2 chars

--- nvim-lua/.config/nvim/lua/plugins/cmp.lua
+++ nvim-lua/.config/nvim/lua/plugins/cmp.lua
@@ -33,8 +33,8 @@ return {
       },
 
       sources = cmp.config.sources({
-        { name = "path" },
-        { name = "nvim_lsp" },
+        { name = "path", keyword_length = 2 },
+        { name = "nvim_lsp", keyword_length = 2},
         { name = "buffer" , keyword_length = 2 },
         { name = "luasnip" , keyword_length = 2 },
       })
