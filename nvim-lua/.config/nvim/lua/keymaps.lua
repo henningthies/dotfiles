874f92694c6e048d0e27877b72d44425f7a937fc
Henning Thies <mail@henning-thies.de>
2024-05-06 08:01:25 +0200

    misc

--- nvim-lua/.config/nvim/lua/keymaps.lua
+++ nvim-lua/.config/nvim/lua/keymaps.lua
@@ -15,13 +15,13 @@ vim.keymap.set("n", "<leader>C", '"+Y', { desc = "copy line" })
 vim.keymap.set("n", "<leader>p", '"+p', { desc = "past below cursor" })
 
 -- write file with <leader>s
-vim.keymap.set("n", "<leader>s", ":w<cr>", { desc = "[s]ave current file"})
+vim.keymap.set("n", "<leader>s", ":w<cr>", { desc = "[s]ave current file" })
 
 -- Switch between current and previous buffer
-vim.keymap.set("n", "<leader><leader>", "<c-^>", { noremap=true, silent=true })
+vim.keymap.set("n", "<leader><leader>", "<c-^>", { noremap = true, silent = true })
 
 -- clear search highlight with space
-vim.keymap.set("n", "<space>", "<cmd> :nohlsearch<Bar>:echo<CR>", { noremap=true, silent=true })
+vim.keymap.set("n", "<space>", "<cmd> :nohlsearch<Bar>:echo<CR>", { noremap = true, silent = true })
 
 -- map insert mode jk to esc
 vim.keymap.set("i", "jk", "<esc>")
@@ -33,38 +33,41 @@ vim.keymap.set("i", "jk", "<esc>")
 --vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "jump right"})
 
 -- NvimTree
-vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<cr>", { silent = true})
-vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<cr>", { silent = true})
+vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<cr>", { silent = true })
+vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<cr>", { silent = true })
 
--- Telescope 
-vim.keymap.set("n", "<leader>f", "<cmd> Telescope git_files<cr>", { silent = true})
-vim.keymap.set("n", "<leader>F", "<cmd> Telescope find_files<cr>", { silent = true})
-vim.keymap.set("n", "<leader>g", "<cmd> Telescope grep_string<cr>", { silent = true})
-vim.keymap.set("n", "<leader>G", "<cmd> Telescope git_status<cr>", { silent = true})
-vim.keymap.set("n", "<leader>b", "<cmd> Telescope buffers<cr>", { silent = true})
-vim.keymap.set("n", "<leader>h", "<cmd> Telescope help_tags<cr>", { silent = true})
-vim.keymap.set("n", "<leader>d", "<cmd> Telescope diagnostics<cr>", { silent = true})
+-- Telescope
+vim.keymap.set("n", "<leader>f", "<cmd> Telescope find_files<cr>", { silent = true })
+vim.keymap.set("n", "<leader>F", "<cmd> Telescope git_files<cr>", { silent = true })
+vim.keymap.set("n", "<leader>g", "<cmd> Telescope grep_string<cr>", { silent = true })
+vim.keymap.set("n", "<leader>G", "<cmd> Telescope git_status<cr>", { silent = true })
+vim.keymap.set("n", "<leader>b", "<cmd> Telescope buffers<cr>", { silent = true })
+vim.keymap.set("n", "<leader>h", "<cmd> Telescope help_tags<cr>", { silent = true })
+vim.keymap.set("n", "<leader>d", "<cmd> Telescope diagnostics<cr>", { silent = true })
 
 --- center view after search
-vim.keymap.set("n", "n", "nzzzv", { noremap=true, silent=true })
-vim.keymap.set("n", "N", "Nzzzv", { noremap=true, silent=true })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap=true, silent=true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap=true, silent=true })
+vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 
 -- create undo breakpoints on .
-vim.keymap.set("i", ".", ".<c-g>u", { noremap=true, silent=true })
+vim.keymap.set("i", ".", ".<c-g>u", { noremap = true, silent = true })
 
 -- close buffer
-vim.keymap.set("n", "<c-x>", ":bd<cr>", { noremap=true, silent=true })
+vim.keymap.set("n", "<c-x>", ":bd<cr>", { noremap = true, silent = true })
 
 -- yanky
-vim.keymap.set({"n","x"}, "p", "<Plug>(YankyPutAfter)")
-vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)")
-vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)")
-vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
+vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
 vim.keymap.set("n", "<leader>z", ":YankyRingHistory<cr>")
 vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
 vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
 
 -- ferret
 vim.keymap.set("n", "<leader>a", "<Plug>(FerretAckWord)")
+
+-- luasnip
+vim.keymap.set({ "i" }, "<C-L>", function() require("luasnip").jump(1) end, { silent = true })
--- nvim-lua/.config/nvim/lua/plugins/cmp.lua
+++ nvim-lua/.config/nvim/lua/plugins/cmp.lua
@@ -36,7 +36,7 @@ return {
         { name = "path", keyword_length = 2 },
         { name = "nvim_lsp", keyword_length = 2},
         { name = "buffer" , keyword_length = 2 },
-        { name = "luasnip" , keyword_length = 2 },
+        { name = "luasnip" , keyword_length = 1 },
       })
     })
 
